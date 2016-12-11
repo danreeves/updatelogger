80252ca82b7373c405c3677cfb90b123fce3a885
Dan Reeves <heydanreeves@gmail.com>
2016-12-11 21:52:22 +0000

    smaller image

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ function Home ({ auth }) {
     return (<div>
         <h1>Home</h1>
         <p>Hi, { auth.user.displayName }</p>
-        <img src={auth.user.photoURL} role="presentation" />
+        <img src={auth.user.photoURL} role="presentation" height="40" width="40" />
     </div>);
 }
 
